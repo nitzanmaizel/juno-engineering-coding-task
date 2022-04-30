84ae07aa0728b1b76ac3c2b740a712acd9e05c1d
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-04-30 17:56:31 +0300

    render first img from fatch api

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { fetchImageUrls } from "../api/index";
 
-const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+const ImageCarousel = () => {
+  const [imagesUrl, setImagesUrl] = useState([]);
+
+  useEffect(() => {
+    const fetchImageFromApi = async () => {
+      try {
+        const imagesUrl = await fetchImageUrls();
+        setImagesUrl(imagesUrl);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchImageFromApi();
+  }, []);
+
+  return (
+    <div className="container">
+      <div>Prev</div>
+      {imagesUrl.length > 0 ? <img src={imagesUrl[0]} alt="img" width={500} height={500} /> : <div>Loading...</div>}
+      <div>Next</div>
+    </div>
+  );
 };
 export default ImageCarousel;
