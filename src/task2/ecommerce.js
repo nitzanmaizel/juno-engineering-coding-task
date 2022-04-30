7350be6c59f26927285cfdcad8fbd79a346bcade
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-04-30 18:06:47 +0300

    task-2/fetchAllOrders - map over ids and return promise all after fetchOrderById fun done

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -1,30 +1,35 @@
 ////////////////////////////////////////////// Helper code, do not edit /////////////////////////////////////////
-import { allIds, fetchOrderById } from "../api";
+import { allIds, fetchOrderById } from '../api';
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
-const fetchAllOrders = () => {
+const fetchAllOrders = async () => {
+  //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+  try {
     const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+    const fetchedOrders = ids.map((id) => fetchOrderById(id));
+    return Promise.all(fetchedOrders);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
-    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
-    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
+  let ordersByUsers = {};
+  //   2. TODO: using the function from section 1 you should now bucket the orders by user.
+  // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+  return ordersByUsers;
 };
 
 const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+  //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
 };
 
 const bucketOrdersByDate = () => {
-    let ordersByDate = {};
-    //   4. TODO: using the function from section 3 bucket the orders by date.
-    // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
+  let ordersByDate = {};
+  //   4. TODO: using the function from section 3 bucket the orders by date.
+  // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+  return ordersByDate;
 };
 
 fetchAllOrders();
